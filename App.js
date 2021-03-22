a5a6e935de08ef4fe94098019f0bc50d2d68706f
muhammad-asher <kmuhammadasher0308@gmail.com>
2021-03-22 22:57:56 +0500

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabs from "./app/navigation/BottomTabs";
+import Login from "./app/screens/Login";
 export default function App() {
 	return (
 		<View style={styles.container}>
