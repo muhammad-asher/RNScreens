d65ee3968c04a0d73470b61bb7c03755b1b51fa4
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-12 11:51:37 +0500

    Update index.js

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -2,3 +2,5 @@ export const homeIcon = require("./home.png");
 export const cartIcon = require("./cart-bag.png");
 export const likeIcon = require("./like.png");
 export const categoriesIcon = require("./category.png");
+export const hamburgerIcon = require("./hamburger.png");
+export const shoppingcartIcon = require("./shoppingcart.png");
