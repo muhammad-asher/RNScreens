28a2ed9e2ce79b2b9ab00f87c9dec10331dc6005
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-12 11:51:25 +0500

    Bulid HomeScreen UI

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,24 @@
 //import liraries
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 // create a component
 function HomeScreen(props) {
 	return (
 		<View style={styles.container}>
-			<Text>Home</Text>
+			<Searchbar
+				style={styles.searchbar}
+				placeholder="Search"
+				iconColor="black"
+			/>
+			<Image
+				style={styles.adImage}
+				source={require("../../assets/Images/Bakingad.png")}
+			/>
+			<View style={styles.card}>
+				<Text style={styles.text}>Shop By Category</Text>
+			</View>
 		</View>
 	);
 }
@@ -14,6 +26,23 @@ function HomeScreen(props) {
 // define your styles
 const styles = StyleSheet.create({
 	container: {},
+	searchbar: {
+		width: "95%",
+		margin: 10,
+		borderRadius: 10,
+	},
+	adImage: {
+		width: "97%",
+		height: 200,
+		margin: 5,
+		borderRadius: 10,
+	},
+	text: {
+		margin: 15,
+		alignSelf: "center",
+		fontSize: 17,
+	},
+	card: {},
 });
 
 //make this component available to the app
