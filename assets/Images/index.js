9776fc384d8beab8e9d0d4e81b71c0cf8b26ea18
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-13 16:17:54 +0500

    Add Images to the Ads Component

---
+++ assets/Images/index.js
@@ -0,0 +1,6 @@
+export const arielsurf = require("./arielsurf.jpg");
+export const butter = require("./butter.jpg");
+export const dettol = require("./dettol.jpg");
+export const furniture = require("./furniture.jpg");
+export const walls = require("./walls.jpg");
+export const eappliances = require("./eappliances.jpg");
