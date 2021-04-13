cc0e0c89acbaca062702fad9ec8c81a68f01f4f1
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-13 16:17:12 +0500

    Add Icons for Categories Card Component

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -4,3 +4,11 @@ export const likeIcon = require("./like.png");
 export const categoriesIcon = require("./category.png");
 export const hamburgerIcon = require("./hamburger.png");
 export const shoppingcartIcon = require("./shoppingcart.png");
+
+export const birthdaycake = require("./birthdaycake.png");
+export const drinks = require("./drinks.png");
+export const fastfood = require("./fastfood.png");
+export const fruits = require("./fruits.png");
+export const cutlery = require("./cutlery.png");
+export const supplies = require("./supplies.png");
+export const vegetable = require("./vegetable.png");
